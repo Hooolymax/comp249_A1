859e3d6bb148c024803aaff5443ed9489f83db0d
Hooolymax <86492968+Hooolymax@users.noreply.github.com>
2024-02-14 21:35:50 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) throws Exception {
 
         System.out.print("It works");
-       
+        System.out.print("Coool");
 
         
     }
