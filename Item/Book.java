2a7bef6797769807ae191e6d63d0679db6ad77dd
Hooolymax <volitantandre@gmail.com>
2024-02-15 23:28:39 -0500

    Update Book.java

--- Item/Book.java
+++ Item/Book.java
@@ -1,5 +1,54 @@
 package Item;
 
+//--------------------------------------------------------------------  
+// Assignment 1
+// Question 
+// Written by Alisa Ignatina 40267100 and Jinghao Lai 40041316 
+//--------------------------------------------------------------------
+
 public class Book extends Item{
+
+    private int numberOfPages; 
+
+
+
+// Default Constructor
+
+public Book(){
+
+    super();
+    this.numberOfPages=0;
+
+}
+
+// Parameterized Constructor
+
+public Book(String id, String name, String author, int yearOfPublication, int numberOfPages){
+
+    super(name,author,yearOfPublication);
+    this.numberOfPages=numberOfPages;
+
+}
+
+
+ // Copy Constructor
+
+ public Book(Book otherBook){
+    super(otherBook);
+    this.numberOfPages=otherBook.numberOfPages;
+
+ }
+
+//getter
+
+
+public int getNmuberOfPages(int numberOfPages){
+    return numberOfPages;
+}
+ 
+
+
+
+
     
 }
